1950aa7e08ed4a7401c61450c32157d7888d93c4
Rajiv Senthilnathan <rajivnathan@gmail.com>
2023-04-20 13:45:52 -0400

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","Totally Awesome National Parks 3", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparks","Totally Awesome National Parks 4", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
